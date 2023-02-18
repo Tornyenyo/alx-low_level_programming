f6c3a70538745d2d2dd5ac32046882b9517152c2
Tornyenyo <tornyenyogad@gmail.com>
2023-02-18 12:42:47 -0800

    final

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * main-
+ * main - alphabets lower and upper
  *
  * Return success(0)
  */
@@ -11,7 +11,10 @@ int main(void)
 	char a;
 
 	for (a = 'A'; a <= 'Z'; a++)
+	{
 		putchar(a);
+		putchar('\n');
+	}
 	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
 
