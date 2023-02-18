d20d0ceb587a473a77abb65fc90ddc362293f377
Tornyenyo <tornyenyogad@gmail.com>
2023-02-18 13:27:24 -0800

    ok now

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-        char a;
+	char a;
 
-        for (a = 'a'; a <= 'z'; a++)
-                putchar(a);
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
 	{
-                putchar('\n');
+		putchar('\n');
 	}
-        return (0);
+	return (0);
 }
