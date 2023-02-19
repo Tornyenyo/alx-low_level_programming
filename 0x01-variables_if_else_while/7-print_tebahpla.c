dffd7c3225ea49051a1083b962f1aa55bfade1f8
Tornyenyo <tornyenyogad@gmail.com>
2023-02-19 01:13:13 -0800

    writing nice codes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main- reverse
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z'; rev <= 'a'; rev--)
+	{
+		putchar(rev);
+	}
+	putchar('\n');
+
+	return (0);
+}
