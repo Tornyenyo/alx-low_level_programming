4ecba9f065ad216c3f4d709bde13810fc2b74f4a
Tornyenyo <tornyenyogad@gmail.com>
2023-02-19 02:18:48 -0800

    ok

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,15 @@ int main(void)
 	for (y = 0; y < 10; y++)
 	{
 		putchar('0' + y);
-		putchar(',');
-	}
-		putchar('\n');
 
+		if (y != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
 	return (0);
 }
 
