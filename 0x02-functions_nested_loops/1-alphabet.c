64cbeb1c626fff6ea2aa5ee7685b961eeb5b7aa5
Tornyenyo <tornyenyogad@gmail.com>
2023-02-21 08:40:12 -0800

    first commit for 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main- Program to display alphabets
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
