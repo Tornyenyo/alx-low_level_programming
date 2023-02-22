19f149518a2eea8394d4760442199b734b0a0089
Tornyenyo <tornyenyogad@gmail.com>
2023-02-22 12:29:55 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adding 2 numbers
+ * @x: the first number
+ * @y: the second number
+ * Return: (n) Success
+ */
+int add(int x, int y)
+{
+	int n;
+
+	n = x + y;
+
+	return (n);
+}
