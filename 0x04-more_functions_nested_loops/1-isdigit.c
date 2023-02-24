0dfc7dc0ca457d83f2167bbae1e6b05a225e3c87
Tornyenyo <tornyenyogad@gmail.com>
2023-02-24 15:07:37 +0000

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * isdigit - fxn that checks for digit(0 - 9)
+ * _isdigit - fxn that checks for digit(0 - 9)
  * @c: the erturn char
  *
  * Return: 0
@@ -11,9 +11,7 @@ int _isdigit(int c)
 	if ((c >= 0) && (c < 10))
 	{
 		return (1);
-	}
 	else
-	{
 		return (0);
 	}
 }
