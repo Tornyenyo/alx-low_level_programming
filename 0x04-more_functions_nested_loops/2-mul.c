019d49034450115c3668135a370bdbac4adcbed3
Tornyenyo <tornyenyogad@gmail.com>
2023-02-24 15:51:09 +0000

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiply 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
