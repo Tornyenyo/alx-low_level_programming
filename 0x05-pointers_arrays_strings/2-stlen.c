cee98f5e313e425d2147a80be5e3f28b04a1f097
Tornyenyo <tornyenyogad@gmail.com>
2023-02-27 23:12:34 +0000

    update

---
+++ 0x05-pointers_arrays_strings/2-stlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns string lenght
+ *  @s: string
+ *
+ *  Return: lenght
+ */
+int _strlen(char *s);
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (0);
+}
