653e51976fc739339068c64e30b55c82c6c61621
Tornyenyo <tornyenyogad@gmail.com>
2023-03-03 04:39:45 +0000

    update8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - fxn to encode strings
+ * @s: ok
+ *
+ * Return: Always
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+{
+	for (j = 0; j < 52; j++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
