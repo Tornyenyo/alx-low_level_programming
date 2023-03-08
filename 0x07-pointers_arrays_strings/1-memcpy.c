dffa31210f668e920a1a39944c97ecad9edc1119
Tornyenyo <tornyenyogad@gmail.com>
2023-03-08 01:28:06 +0000

    ok

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - copies memeory area
+ * @dest: destination
+ * @src: source
+ * @n: lenght of source
+ *
+ * Return: Always
+ */
+char *_memcpy(char *dest, char *src, unisgned int n)
+{
+	unsigned int i;
+
+	for ( i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return(dest);
+}
