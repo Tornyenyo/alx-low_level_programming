1d314b59f27e198e4015ff78e3bfe5d17b662c8d
Tornyenyo <tornyenyogad@gmail.com>
2023-03-08 01:30:26 +0000

    okay

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,9 @@ char *_memcpy(char *dest, char *src, unisgned int n)
 {
 	unsigned int i;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	return(dest);
+	return (dest);
 }
