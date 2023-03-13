dad6c431741d862c48a8d997b9829f6f28ce8567
Tornyenyo <tornyenyogad@gmail.com>
2023-03-13 13:58:13 +0000

    ok

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print the name of teh program the a new line
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
