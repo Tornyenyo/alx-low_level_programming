2f7bc4cd3f1ada986d09278b16de8afcb656937a
Tornyenyo <tornyenyogad@gmail.com>
2023-03-15 22:54:21 +0000

    difficult

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,17 +17,17 @@ char *str_concat(char *s1, char *s2)
 
 	s1_len = s2_len = 0;
 	len = s1_len + s2_len;
-	if (s1 == NULL)
-		s1 = "";
-	s1_len = strlen(s1);
+	if (s1 != NULL)
+	{
 	while (s1[s1_len] != '\0')
 		s1_len++;
+	}
 
-	if (s2 == NULL)
-		s2 = "";
-	s2_len = strlen(s2);
+	if (s2 != NULL)
+	{
 	while (s2[s2_len] != '\0')
 		s2_len++;
+	}
 
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
