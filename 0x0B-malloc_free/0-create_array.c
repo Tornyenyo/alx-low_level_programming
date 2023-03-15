7bdf0d788754fe223fa3107e36dbfc1b6dbb930e
Tornyenyo <tornyenyogad@gmail.com>
2023-03-15 20:15:50 +0000

    betty style error L14

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char*)malloc(size * sizeof(char));
+	arr = (char *)malloc(size * sizeof(char));
 
 	if (size == 0)
 	{
