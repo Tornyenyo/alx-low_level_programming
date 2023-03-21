af966b8baf294623f71e1c1134a3ccb8184b09a9
Tornyenyo <tornyenyogad@gmail.com>
2023-03-21 02:55:57 +0000

    corrected wrong prototype

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
 
 /**
- * _init_dog - initialize a struct(type dog)
+ * init_dog - initialize a struct(type dog)
  * @d: pointer to sructure
  * @name: pointer to name of dog
  * @age: age of dog
  * @owner: pointer to owner
  *
- * Return:
+ * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
