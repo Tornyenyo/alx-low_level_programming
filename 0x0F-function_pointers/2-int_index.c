5e959b81117c86252c5cf398c5a6084e0ead6308
Tornyenyo <tornyenyogad@gmail.com>
2023-03-22 22:35:31 +0000

    error correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,9 +13,11 @@ int int_index(int *array, int size, int (*cmp)(int));
 	int i;
 
 	if (array != NULL && cmp != NULL)
+	{
 		for (i = 0; i < size, i++)
 		{
-			if (cmp(array[i])
+			if (cmp(array[i]))
 				return (i);
 		}
-
+	}
+}
